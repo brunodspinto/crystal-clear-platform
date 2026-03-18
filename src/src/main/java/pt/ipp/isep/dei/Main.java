f068dfc2f9b2e12223e6c7a2665b0765fe3888e9
tomasfonseca <1251514@isep.ipp.pt>
2026-03-18 11:06:18 +0000

    test commit

--- src/src/main/java/pt/ipp/isep/dei/Main.java
+++ src/src/main/java/pt/ipp/isep/dei/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.run();
-
+        System.out.println(".");
         try {
             MainMenuUI menu = new MainMenuUI();
             menu.run();
