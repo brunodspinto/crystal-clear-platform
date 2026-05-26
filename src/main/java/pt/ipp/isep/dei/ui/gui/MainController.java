addda86afd420db200ecf9300213cd9b7bd2539f
tomasfonseca <1251514@isep.ipp.pt>
2026-05-26 18:49:16 +0100

    Added the initial files for the menu to boot up

---
+++ src/main/java/pt/ipp/isep/dei/ui/gui/MainController.java
@@ -0,0 +1,58 @@
+package pt.ipp.isep.dei.ui.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Mediator that owns the root BorderPane and is responsible for swapping
+ * the center region whenever the user navigates between scenes. Each child
+ * controller communicates with the rest of the GUI through this class so
+ * scenes do not need to know about each other directly.
+ */
+public class MainController {
+
+    @FXML
+    private BorderPane root;
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void showLogin() {
+        loadCenter("/fxml/Login.fxml");
+    }
+
+    public void showCitizenMenu() {
+        loadCenter("/fxml/CitizenMenu.fxml");
+    }
+
+    public void showJournalistMenu() {
+        loadCenter("/fxml/JournalistMenu.fxml");
+    }
+
+    private void loadCenter(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent view = loader.load();
+
+            Object child = loader.getController();
+            if (child instanceof LoginController) {
+                ((LoginController) child).setMainController(this);
+            }
+
+            root.setCenter(view);
+        } catch (Exception e) {
+            // se a fxml ainda nao existir, regista no log pa nao crashar a app toda
+            e.printStackTrace();
+        }
+    }
+}
